f44093a10be5bfb56f8c4cdc378bf08c7479f5c7
Nathaniel Grevatt <nqg3ux@virginia.edu>
2016-09-11 23:06:30 -0400

    Categor and Task models

--- models/src/models/app/models.py
+++ models/src/models/app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
 
 
@@ -23,5 +24,15 @@ class ServiceProvider(models.Model):
     user = models.OneToToneField(User, primary_key=True)
     is_verified = models.BooleanField(default=False)
 
-# Category (N)
-# Task (N)
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+class Task(models.Model):
+    user = models.OneToOneField(User, primary_key=True)
+    category = models.ForeignKey(Category)
+    description = models.TextField()
+    cost = models.IntField()
+    created_date = models.DateTimeField(default=timezone.now)
+    due_date = models.DateTimeFIeld()
+    complete = models.BooleanField(default=False)
+    
