06223ba3c19324a16b9fda81df5c98cece6d6e8a
Ethan Lowman <ethan.uva@ethanlowman.com>
2016-09-19 02:02:49 -0400

    Config fixes

--- models/src/models/settings.py
+++ models/src/models/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'models.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cs4501',
+        'USER': 'www',
+        'PASSWORD': '$3cureUS',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
@@ -101,6 +105,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "collected_static")
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
