3717a15f750d461a3051544b2448e507590349d0
Nathaniel Grevatt <nqg3ux@virginia.edu>
2016-10-05 19:37:43 -0400

    TaskDetailPage view and url

--- exp/exp/app/views.py
+++ exp/exp/app/views.py
@@ -7,3 +7,9 @@ class CategoriesPage(APIView):
     def get(self, request):
         r = requests.get("http://models:8002/api/v1/category/")
         return Response(r.json())
+
+class TaskDetailPage(APIView):
+    def get(self, request):
+        payload = {'id': request.GET("id", None)}
+        r = requests.get("http://models:8002/api/v1/task/", params=payload)
+        return Response(r.json())
--- exp/exp/urls.py
+++ exp/exp/urls.py
@@ -16,5 +16,7 @@ from django.conf.urls import include, url
 from .app.views import *
 
 urlpatterns = [
-    url(r"^CategoriesPage$", CategoriesPage.as_view())
+    url(r"^CategoriesPage$", CategoriesPage.as_view()),
+    url(r"^TaskDetail$", TaskDetail.as_view())
+
 ]
