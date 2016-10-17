e60e8571e29e9935a65e6527a54bc83668d72563
Ethan Lowman <ethan.uva@ethanlowman.com>
2016-10-16 22:03:35 -0400

    Add password field

--- models/models/app/models.py
+++ models/models/app/models.py
@@ -5,6 +5,8 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class User(models.Model):
     username = models.CharField(max_length=128)
+    password = models.CharField(max_length=128)
+
     first_name = models.CharField(max_length=128)
     last_name = models.CharField(max_length=128)
     email = models.EmailField()
