fd6805995e9700da5bf1ee5a8d8bb1f21de9b05a
Ethan Lowman <ethan.uva@ethanlowman.com>
2016-10-19 19:50:58 -0400

    Logout models view

--- models/models/app/views.py
+++ models/models/app/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse, HttpResponse
 from django.core import exceptions
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, views
 from .serializers import *
 from .models import *
 
@@ -46,28 +46,43 @@ class TaskViewSet(ListAsDictMixin, viewsets.ModelViewSet):
     filter_fields = ("id", "category",)
 
 
-@csrf_exempt
-def login_view(request):
-    username = request.POST.get("username", "")
-    password = request.POST.get("password", "")
-    if username == "" or password == "":
+class LoginView(views.APIView):
+    def post(self, request):
+        username = request.POST.get("username", "")
+        password = request.POST.get("password", "")
+        if username == "" or password == "":
+            return JsonResponse({
+                "error": "must pass username and password"
+            }, status=400)
+
+        login_failure = JsonResponse({
+            "error": "invalid username or password"
+        })
+        try:
+            user = User.objects.get(username=username)
+        except exceptions.ObjectDoesNotExist:
+            return login_failure
+
+        token = user.login(password)
+        if token is None:
+            return login_failure
         return JsonResponse({
-            "error": "must pass username and password"
-        }, status=400)
+            "token": token.token
+        })
 
-    login_failure = JsonResponse({
-        "error": "invalid username or password"
-    })
-    try:
-        user = User.objects.get(username=username)
-    except exceptions.ObjectDoesNotExist:
-        return login_failure
 
-    token = user.login(password)
-    if token is None:
-        return login_failure
-    return JsonResponse({
-        "token": token.token
-    })
+class LogoutView(views.APIView):
+    def post(self, request):
+        token = request.POST.get("token", "")
+        if token == "":
+            return JsonResponse({
+                "error": "must pass token"
+            }, status=400)
+
+        AuthenticationToken.objects.filter(token=token).delete()
+
+        return JsonResponse({
+            "status": "OK"
+        })
 
 
