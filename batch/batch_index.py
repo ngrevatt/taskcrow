aad0f2aebbdd92674e2e986f2bbad072a81876ab
Ethan Lowman <ethan.uva@ethanlowman.com>
2016-11-09 20:04:22 -0500

    Index existing models

--- batch/batch_index.py
+++ batch/batch_index.py
@@ -21,18 +21,34 @@ while True:
     try:
         r = requests.get("http://es:9200/")
     except:
-        time.sleep(5)
+        pass
     else:
         if r.status_code == 200:
             break
+    time.sleep(5)
 
 es = Elasticsearch(['es'])
-if not es.indices.exists("listing_index"):
-    es.indices.create(index='listing_index')
+es.indices.create(index='listing_index', ignore=400)
 
+
+# Preload index
+while True:
+    try:
+        r = requests.get("http://models/api/v1/task/")
+    except Exception as e:
+        pass
+    else:
+        if r.status_code == 200:
+            break
+    time.sleep(5)
+
+for record in r.json()["records"]:
+    es.index(index='listing_index', doc_type='listing', id=record["id"], body=record)
+es.indices.refresh(index="listing_index")
+
+# Digest queue
 for message in consumer:
     listing = json.loads((message.value).decode('utf-8'))
     print(listing)
     es.index(index='listing_index', doc_type='listing', id=listing['id'], body=listing)
-
     es.indices.refresh(index="listing_index")
