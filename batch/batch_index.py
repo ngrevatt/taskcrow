19ac536f73005e05c583bbdea895b26969cd4499
Ethan Lowman <ethan.uva@ethanlowman.com>
2016-11-10 20:44:32 -0500

    Mock progress

--- batch/batch_index.py
+++ batch/batch_index.py
@@ -6,49 +6,59 @@ from kafka import KafkaConsumer
 from kafka.common import NodeNotReadyError
 
 
-def init_consumer():
+class BatchJob(object):
+    def __init__(self, consumer, es):
+        self.consumer = consumer
+        self.es = es
+
+    def run(self):
+        while True:
+            try:
+                r = requests.get("http://es:9200/")
+            except:
+                pass
+            else:
+                if r.status_code == 200:
+                    break
+            time.sleep(5)
+
+        self.es.indices.create(index='listing_index', ignore=400)
+
+
+        # Preload index
+        while True:
+            try:
+                r = requests.get("http://models/api/v1/task/")
+            except Exception as e:
+                pass
+            else:
+                if r.status_code == 200:
+                    break
+            time.sleep(5)
+
+        for record in r.json()["records"]:
+            self.es.index(index='listing_index', doc_type='listing', id=record["id"], body=record)
+        es.indices.refresh(index="listing_index")
+
+        # Digest queue
+        for message in self.consumer:
+            listing = json.loads((message.value).decode('utf-8'))
+            print(listing)
+            self.es.index(index='listing_index', doc_type='listing', id=listing['id'], body=listing)
+            self.es.indices.refresh(index="listing_index")
+
+
+if __name__ == "__main__":
+    consumer = None
     while True:
         try:
             consumer = KafkaConsumer('new-listings-topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
         except NodeNotReadyError:
             time.sleep(5)
         else:
-            return consumer
-
-consumer = init_consumer()
-
-while True:
-    try:
-        r = requests.get("http://es:9200/")
-    except:
-        pass
-    else:
-        if r.status_code == 200:
             break
-    time.sleep(5)
 
-es = Elasticsearch(['es'])
-es.indices.create(index='listing_index', ignore=400)
+    es = Elasticsearch(['es'])
+    b = BatchJob(consumer, es)
+    b.run()
 
-
-# Preload index
-while True:
-    try:
-        r = requests.get("http://models/api/v1/task/")
-    except Exception as e:
-        pass
-    else:
-        if r.status_code == 200:
-            break
-    time.sleep(5)
-
-for record in r.json()["records"]:
-    es.index(index='listing_index', doc_type='listing', id=record["id"], body=record)
-es.indices.refresh(index="listing_index")
-
-# Digest queue
-for message in consumer:
-    listing = json.loads((message.value).decode('utf-8'))
-    print(listing)
-    es.index(index='listing_index', doc_type='listing', id=listing['id'], body=listing)
-    es.indices.refresh(index="listing_index")
